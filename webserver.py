66a19565324ee3624b85b6c51fb805dbd0e3846e
aureliengregoire94 <60916671+tributZ@users.noreply.github.com>
2022-02-14 16:38:58 +0100

    commit

---
+++ webserver.py
@@ -0,0 +1,31 @@
+from asyncio import subprocess
+from unittest import result
+from urllib import *
+from urllib import request
+from flask import Flask
+from json import *
+
+@app.route("/request", methods=["POST"]):
+def get_request():
+    data = request.get_json()
+    print(data)
+    url=data['url']
+    for site in sites: 
+        if url.startswith(site):
+            path = url
+            website= site
+    
+    cmd+=path
+    subprocess.call(cmd, shell=True)
+    return 'ok' if len(result) == 1 else 'Error'
+
+@app.route('url_check', methods=["POST"]
+def check_url():
+    url=request.get_json()['url']
+    for site in sites:
+        if url.startswith(site):
+            ret = 'Ok'
+        else 
+            ret = 'No'
+    return jsonify(ret)
+
