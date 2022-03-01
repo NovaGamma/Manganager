aa0d2ee2c07ffeb7b59e24b25afd719648281f7f
Elvin Aupiais--Berthy <gammanova41@gmail.com>
2022-03-01 13:56:45 +0100

    add 3 sites support

--- mangakakalot.py
+++ mangakakalot.py
@@ -5,20 +5,23 @@ from utils import clean
 def url_scheme():
     return "https://mangakakalot.com/manga-"
 
-RAW_URL = "https://readmanganato.com/manga-mh989642/chapter-136"
-temp = RAW_URL.split("/")
-URL = "/".join(temp[:-1]) +"/"
-print (URL)
+URL = "https://mangakakalot.com/chapter/nc923742/chapter_237"
 
 r = requests.get(URL)
 if r.status_code == 200:
     soup = BeautifulSoup(r.text, "html.parser")
-    chapterList = soup.find_all("li", class_="a-h")
-    for li in chapterList:
-        a = clean(li.contents)[0]
-        url = a.attrs['href']
+    new_link = soup.find('div', class_ = "breadcrumb breadcrumbs bred_doc")
+    bread = clean(clean(clean(new_link.contents)[0].contents)[2].contents)[0]
+    title = bread.attrs['title']
+    link = bread.attrs['href']
+    print(title, link)
+
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, "html.parser")
+    chapter_list = soup.find('div', class_ = "chapter-list")
+    chapter_list = clean(chapter_list.contents)
+    for chapter in chapter_list:
+        a = clean(chapter.contents)[0].contents[0]
         chapter_name = a.contents[0]
+        url = a.attrs['href']
         print(chapter_name, url)
-    name = clean(soup.find('div', class_ = "panel-breadcrumb").contents)
-    title = name[2].attrs['title']
-    print(title)
