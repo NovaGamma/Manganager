629f2f7c5fcd39aa9a88732eabca5f055b796104
Elvin Aupiais--Berthy <gammanova41@gmail.com>
2022-03-27 11:47:58 +0200

    test for the chapterList

--- Manganager/src/App.vue
+++ Manganager/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestChapter/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestChapter from './components/TestChapter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestChapter
   }
 }
 </script>
