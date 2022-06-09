70d1cebeb383af6994afc589b9e4cb456cae7a11
NovaGamma <gammanova41@gmail.com>
2022-06-09 13:08:30 +0200

    fix popup script

--- extension/popup-script.js
+++ extension/popup-script.js
@@ -1,4 +1,15 @@
-import get_site from './utils'
+function get_site(url){
+  if (url.match(/https:\/\/mangatx\.com\/manga\/.+\/.+\//)){
+    return 'mangatx'
+  }
+  else if(url.match(/https:\/\/readmanganato\.com\/manga.+\/chapter-.+/)){
+    return 'readmanganato'
+  }
+  else if(url.match(/https:\/\/mangakakalot\.com\/chapter\/.+\/.+/)){
+    return 'mangakakalot'
+  }
+  else return 'undefined'
+}
 
 var query = { active: true, currentWindow: true };
 chrome.tabs.query(query, async function(tabs){
